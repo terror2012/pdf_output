95e178ffd7930ba9fd743cc9aa7eba24c9a8c3c1
Cojocari Cristian <megaimpact2012@yahoo.com>
2017-04-28 17:23:05 +0300

    add new forms + getting new form array

--- core_code.php
+++ core_code.php
@@ -38,23 +38,22 @@ class core_code
         {
             $name = key($stuffArray);
             $numberArray = explode('_', $name);
-            $keyNumbers = $numberArray['1'];
+            array_push($keyNumbers, $numberArray['1']);
             next($stuffArray);
         }
         return $keyNumbers;
     }
     function get_array_amount_value($keyNumbers)
     {
-        $arraySet = [];
+        $itemset = [];
         for($i = 0; $i < count($keyNumbers); $i++)
         {
             $key = $keyNumbers[$i];
-            $itemset['servicedescription_'.$key] = $_POST['servicedescription_'.$key];
-            $itemset['netamount_'.$key] = $_POST['netamount_'.$key];
-            $itemset['vatcode_'.$key] = $_POST['vatcode_'.$key];
-            $itemset['vatamount_'.$key] = $_POST['vatamount_'.$key];
-            array_push($arraySet, $itemset);
+            $itemset[$key]['servicedescription_'.$key] = $_POST['servicedescription_'.$key];
+            $itemset[$key]['netamount_'.$key] = $_POST['netamount_'.$key];
+            $itemset[$key]['vatcode_'.$key] = $_POST['vatcode_'.$key];
+            $itemset[$key]['vatamount_'.$key] = $_POST['vatamount_'.$key];
         }
-        return $arraySet;
+        return $itemset;
     }
 }
--- html_form.php
+++ html_form.php
@@ -7,12 +7,14 @@
  */
 
 require_once('core_code.php');
+require_once('vendor/spipu/html2pdf/html2pdf.class.php');
 
 $core = new core_code();
+$pdf_convert = new HTML2PDF();
 
 $patern = '/netamount_.*/';
 $stuffArray =  $core->preg_grep_keys($patern, $_POST);
 $keyNumbers = $core->get_amount_number($stuffArray);
 
 $arraySet = $core->get_array_amount_value($keyNumbers);
-print_r($arraySet);
+print_r($_POST);
